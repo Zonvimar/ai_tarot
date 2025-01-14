0b8a46687df05cfebd768f7a6b55d40302bc68d0
zonvimar <shamanolegan@gmail.com>
2025-01-14 19:42:00 +0300

    fixes

--- src/configs/http-service/constants/baseUrl.ts
+++ src/configs/http-service/constants/baseUrl.ts
@@ -1,5 +1,5 @@
-const DEV_BASE_URL = 'https://esokey.online/'
-const PROD_BASE_URL = 'https://aitarot.io/'
+const DEV_BASE_URL = 'https://api.aitarot.io/'
+const PROD_BASE_URL = 'https://api.aitarot.io/'
 
 export const baseUrls = {
     'prod':PROD_BASE_URL,
